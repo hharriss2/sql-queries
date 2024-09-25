16e31a6e8ac67f50d38b4ead785ceaa81380a5cb
Harris <harris.jones@dorelusa.com>
2024-09-25 12:15:55 -0500

    added last week column booleans

--- ships/ships_view.sql
+++ ships/ships_view.sql
@@ -2,7 +2,7 @@
 create or replace view ships_schema.ships_view as 
 (
 SELECT 
-    id
+    s.id
     ,s.model
     ,COALESCE(msl.item_id::text, s.tool_id::text) AS tool_id
     ,cbm.cat
@@ -21,9 +21,27 @@ SELECT
     ,date_shipped
     ,sales
     ,units
-    ,division
+    ,s.division
     ,d.division_id
     ,coalesce(msl.product_name, s.product_name) as product_name
+    ,w.wm_date
+    ,case
+		when date_shipped >= date_trunc('month',current_date - interval '13 months')
+		and date_trunc('month',date_shipped) !=  date_trunc('month',current_date)
+		then 1 else 0
+		end as is_l52 -- boolean to show last 52 weeks sold
+	,case
+		when date_shipped >= date_trunc('week', current_date - interval '5 weeks')
+		and date_trunc('week',date_shipped) !=date_trunc('week',current_date)
+		then 1
+		else 0
+		end as is_l4 -- boolean to show last 4 weeks sold
+	,case
+		when date_shipped >= date_trunc('week', current_date - interval '13 weeks')
+		and date_trunc('week',date_shipped) !=date_trunc('week',current_date)
+		then 1
+		else 0
+		end as is_l12 -- boolean to show if is last 12 for units sold
 FROM ships_schema.ships s
 join divisions d
 on s.division = d.division_name
@@ -39,6 +57,8 @@ left join account_manager_cat ac
 on cbm.cat = ac.category_name
 left join group_ids g 
 on msl.item_id = g.tool_id
+left join wm_calendar w 
+on s.date_shipped = w.date
 )
 ;
 

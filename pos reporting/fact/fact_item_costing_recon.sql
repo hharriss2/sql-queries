e209400ae3e834dece57d33563de799f9b7a5dba
Harris <harris.jones@dorelusa.com>
2024-10-03 09:24:18 -0500

    renaming cost columns. adding wm cal id

--- pos reporting/fact/fact_item_costing_recon.sql
+++ pos reporting/fact/fact_item_costing_recon.sql
@@ -8,12 +8,15 @@ select dr.*
 	,(ic.material_cost * qty) as material_cost
 	,(ic.freight_cost * qty) as freight_cost
 	,(ic.overhead_cost * qty) as overhead_cost
-	,ic.contribution_profit_cost * qty as cogs
-	,ic.contribution_profit_cost_overhead * qty as cogs_overhead
-	,order_total + (commission_amt) - (rate_amount) as contribution_profit
+	,ic.contribution_profit_cost * qty as net_cost
+	-- ,ic.contribution_profit_cost_overhead * qty as cogs_overhead
+	,order_total + (commission_amt) - (rate_amount) as cogs
+    ,wcv.wmcal_id
 from pos_reporting.dsv_orders_3p_recon dr 
 left join components.item_costing_view ic
 on dr.model = ic.model
+left join power_bi.wm_calendar_view wcv
+on dr.order_date = wcv.date
 where 1=1
 and status !='Refund' -- including refunds will throw off %'s
 )
@@ -29,8 +32,11 @@ select
 	,rate_amount
     ,is_suppression_model
 	,cogs
-	,cogs_overhead
-	,contribution_profit - cogs as  contribution_profit
+    ,net_cost
+--	,cogs_overhead
+	,cogs - net_cost as contribution_profit
+    ,wmcal_id
+
 from rc
 )
 select 
@@ -42,12 +48,14 @@ model
 	,commission_amt
 	,rate_amount
 	,cogs
-	,cogs_overhead
+--	,cogs_overhead
+	,net_cost
 	,contribution_profit
-	,cast(contribution_profit / cogs as numeric(10,2)) as cp_perc
-	,cast(contribution_profit / cogs_overhead as numeric(10,2)) as cp_overhead_perc
+    ,cast(contribution_profit / cogs as numeric(10,2)) as cp_perc
     ,cast(contribution_profit/ qty  as numeric(10,2)) as avg_contribution_profit 
+	-- ,cast(contribution_profit / cogs_overhead as numeric(10,2)) as cp_overhead_perc
     ,is_suppression_model
+    ,wmcal_id
 from details
 )
 ;

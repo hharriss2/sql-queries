74e0df9b6d08ec675a216072a3cd02f8a996b9a2
Harris <harris.jones@dorelusa.com>
2024-11-20 11:06:19 -0600

    adding source for projection report to use inventory

---
+++ projections v2/inventory_feed_com.sql
@@ -0,0 +1,14 @@
+--adding inventory levels to the projections tool
+
+create or replace view projections.inventory_feed_com as 
+(
+select
+	model
+	,sum(feed_quantity) as total_feed
+	,max(date_created) as latest_created_date
+	,max(date_updated) as latest_updated_date
+	,max(inserted_at) as inserted_at
+from pos_reporting.inventory_feeds
+group by model
+)
+;

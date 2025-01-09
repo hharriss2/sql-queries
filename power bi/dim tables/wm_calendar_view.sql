9bbcfd545cc05bd711b0a22768974a648a9e907f
Harris <harris.jones@dorelusa.com>
2025-01-09 10:46:52 -0600

    added wtd and last 4 week booleans

--- power bi/dim tables/wm_calendar_view.sql
+++ power bi/dim tables/wm_calendar_view.sql
@@ -1,6 +1,5 @@
 --view used for power bi reports
-create or replace view power_bi.wm_calendar_view as 
-(
+create or replace view power_bi.wm_calendar_view as (
 with wo as --week order 
 (
 select * 
@@ -46,9 +45,33 @@ SELECT
 				--week 53 is always labeled 53 if not present in the current calendar
         ,date_part('month',date) as month_num
         ,month_seq as current_month_seq
+        ,dense_rank() over (partition by w.wm_date::integer order by date) as wm_day_of_week 
+        -- ^order for the day of the week. saturday = 1
+        ,case -- when the date is today, apply wm date to every row, then compare wm date
+        	when max(case when current_date = date then wm_date else null end) over() = wm_date
+        	then 1
+        	else 0 
+        	end as is_current_wm_week
+        --^if the date is in the current wm week, then 1 else 0
+        ,case -- take the wm date from a week ago and compare it to the wm date for each row
+        	when max(case when current_date - interval '7 days'= date  then wm_date else null end) over() = wm_date
+        	then 1 
+        	else 0
+        	end  as previous_wm_week
+        ,case -- take the wm date from 4 weeks ago and compare it to every row
+        	when max(case when current_date - interval '28 days' = date then wm_date else null end ) over() <=wm_date
+        		and max(case when current_date = date then wm_date else null end) over() != wm_date
+        		--^disclude the current wm date and only look at the past 4
+        	then 1
+        	else 0
+        	end as is_last_4_weeks
+        --finds the wm date before the current week
 FROM wm_calendar w
 left join wo
 on w.wm_week::integer = wo.wm_week
 left join mo 
 on date_part('month',w.date) = mo.month_num
-);
+where 1=1
+and date <=current_date
+order by date desc
+)

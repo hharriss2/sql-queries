b8eb4e9d0cc4a38d60f27d15ef0eedcca7f924e3
Harris <harris.jones@dorelusa.com>
2025-01-23 10:32:47 -0600

    added inventory numbers

--- pos reporting/wm_stores_pos.sql
+++ pos reporting/wm_stores_pos.sql
@@ -35,6 +35,35 @@ from group_ids
 select * 
 from lookups.item_status_store
 )
+,sia as  -- store inventory aggregate
+( -- dataset is store inventory by store number. this clause aggregates it to the item level
+select
+	walmart_item_number
+	,item_name
+    ,business_date
+	,sum(store_in_transit_quantity_this_year) as store_in_transit_quantity_this_year
+	,sum(store_in_warehouse_quantity_this_year) as store_in_warehouse_quantity_this_year
+	,sum(store_on_hand_quantity_this_year) as store_on_hand_quantity_this_year
+	,sum(store_on_order_quantity_this_year) as store_on_order_quantity_this_year
+	,sum(traited_store_count_this_year) as traited_store_count_this_year
+from inventory.wm_store_on_hands
+where business_date = (select max(business_date) from inventory.wm_store_on_hands)
+--^ use the inventory levels from today
+group by walmart_item_number
+	,item_name
+    ,business_date
+)
+,si as --store inventory
+(
+select *
+	,+store_in_warehouse_quantity_this_year
+	+store_on_hand_quantity_this_year
+	+store_on_order_quantity_this_year as stores_on_hand
+from sia
+)
+,details as 
+( -- initial joins before adding inventory
+    --didn't want to have joins with coalesce, so seperating out for cleaner reading
 select 
     ssa.id
     ,ssa.pos_qty
@@ -61,6 +90,7 @@ select
     ,g.collection_name
     ,mcl.is_lookup_update
     ,mcl.is_scrape_product_name
+    ,mcl.is_top_100_item
 from ssa
 left join mcl
 on ssa.prime_item_nbr = mcl.item_id
@@ -70,5 +100,43 @@ left join ca
 on cbm.cat = ca.category_name
 left join g g
 on mcl.item_id = g.tool_id
+)
+select 
+    id
+    ,pos_qty
+    ,pos_sales
+    ,curr_repl_instock
+    ,sale_date
+    ,item_id
+    ,cbm_id
+    ,am_id
+    ,division
+    ,retail_type_id
+    ,product_name
+    ,model
+    ,base_id
+    ,brand_name
+    ,item_stat_id
+    ,item_status
+    ,cat
+    ,sub_cat
+    ,account_manager
+    ,category_id
+    ,group_id
+    ,group_id_id
+    ,collection_name
+    ,is_lookup_update
+    ,is_scrape_product_name
+    ,is_top_100_item
+    ,si.store_in_transit_quantity_this_year
+    ,si.store_in_warehouse_quantity_this_year
+    ,si.store_on_hand_quantity_this_year
+    ,si.store_on_order_quantity_this_year
+    ,si.stores_on_hand
+    ,si.traited_store_count_this_year
+from details
+left join si
+on details.item_id = si.walmart_item_number 
+and details.sale_date = si.business_date
 )
 ;

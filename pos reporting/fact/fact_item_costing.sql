5cf6065fa34d318a5eeed8f7db97b3864c730b50
Harris <harris.jones@dorelusa.com>
2025-03-20 16:29:34 -0500

    addded new shipping source for the item costing tbl

--- pos reporting/fact/fact_item_costing.sql
+++ pos reporting/fact/fact_item_costing.sql
@@ -10,7 +10,7 @@ from components.item_costing_view
 ,isc as --item shipping cost
 ( -- pull in estimated shipping costs 
 select * 
-from components.item_shipping_cost
+from components.item_shipping_cost_tbl
 )
 ,sl as --suppression list
 (
@@ -22,6 +22,17 @@ from lookups.model_suppression_list
 select model
 from clean_data.current_wm_catalog_3p
 where item_status = 'PUBLISHED'
+)
+,avgsc as --average shipping cost
+(
+select
+	model
+	,rate_zone
+	,cast(avg(rate_amount) as numeric(10,2)) avg_actual_rates
+	,count(model) as total_models_shipped
+from POS_REPORTING.tracking_rates_view
+group by model
+	,rate_zone
 )
 ,details as --one more clause bc postgres doesnt let me call alias names without it 
 ( -- joins to pull in item ids, do some math for break even point
@@ -37,23 +48,23 @@ select
 	,isc.length
 	,isc.width
 	,isc.height
-	,isc.weight
+	,isc.weight::numeric
 	,isc.zone_number
-	,isc.base_residential_rate
-	,isc.additional_weight_surcharge
-	,isc.oversize_surcharge
-	,isc.fuel_surcharge
-	,isc.additional_handle_surcharge
+	,null::numeric as base_residential_rate
+	,null::numeric as additional_weight_surcharge
+	,null::numeric as oversize_surcharge
+	,null::numeric as fuel_surcharge
+	,null::numeric as additional_handle_surcharge
 	,material_cost
 	,duty_cost
 	,freight_cost
 	,overhead_cost
-	,isc.total_shipping_cost
+	,isc.shipping_cost::numeric as total_shipping_cost
 	,contribution_profit_cost as net_cost
 	-- ,contribution_profit_cost_overhead
-	,cast((total_shipping_cost + contribution_profit_cost) /.85 as numeric(10,2)) as break_even
+	,cast((shipping_cost + contribution_profit_cost) /.85 as numeric(10,2)) as break_even
 	-- ,cast((total_shipping_cost + contribution_profit_cost_overhead) /.85 as numeric(10,2)) as contribution_break_even_overhead
-	,cast(((contribution_profit_cost * 1.2) + total_shipping_cost) / .85 as numeric(10,2)) as msrp
+	,cast(((contribution_profit_cost * 1.2) + shipping_cost) / .85 as numeric(10,2)) as msrp
 	,case
 		when sl.model is not null
 		then 1
@@ -65,6 +76,11 @@ select
 		then 1
 		else 0
 		end as is_3p_model
+	,avgsc.avg_actual_rates -- average cost of shipping for the model by zone
+	,avgsc.total_models_shipped -- number of times model has shipped by zone
+	,cast((avg_actual_rates + contribution_profit_cost) /.85 as numeric(10,2)) as break_even_actual_avg
+	-- ,cast((total_shipping_cost + contribution_profit_cost_overhead) /.85 as numeric(10,2)) as contribution_break_even_overhead
+	,cast(((contribution_profit_cost * 1.2) + avg_actual_rates) / .85 as numeric(10,2)) as msrp_actual_avg
 from isc
 left join ic
 on ic.model = isc.model
@@ -76,6 +92,9 @@ left join sl
 on isc.model = sl.model
 left join mpl
 on isc.model = mpl.model
+left join avgsc
+on avgsc.model = isc.model
+and avgsc.rate_zone = isc.zone_number::integer
 )
 --final results. last calc to find estimated commission ( 15% of break even )
 select 
@@ -109,6 +128,12 @@ select
 	,is_suppression_model
     ,is_multi_box
 	,is_3p_model
+	,break_even_actual_avg *.15::numeric(10,2) as est_commission_actual
+	,break_even_actual_avg
+	,msrp_actual_avg
+	,avg_actual_rates
+	,avg(avg_actual_rates) over (partition by model) as avg_overall_shipping_cost_actual
+	,total_models_shipped
 from details
 )
 ;
